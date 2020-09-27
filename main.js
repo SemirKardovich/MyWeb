7d660cfbd84679841639cb8d0621a2b6be072726
Semir Kardovikj <semir.k1612@gmail.com>
2020-09-27 21:29:17 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+const navEl = document.getElementById("nav");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+})
+
+
+document.addEventListener("scroll", ()=>{
+    const scrollPosition = window.scrollY;
+    if(scrollPosition > 250){
+        navEl.style.backgroundColor = " rgba(0, 0, 0, 0.9)"; 
+    }else{
+        navEl.style.backgroundColor = "transparent";   
+    }
+})
+//Close navigation on click
+navLink.forEach(function(link){
+    link.addEventListener("click", function(){
+        hamburger.classList.toggle("active");
+        navLinks.classList.toggle("active");
+    })
+});
+
+// Smooth scroll
+
+let scroll = new SmoothScroll('.nav-links a[href*="#"]', {
+    speed:800
+});
